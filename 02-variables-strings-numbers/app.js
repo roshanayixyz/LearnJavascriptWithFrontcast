9ed883af36456a6f1eb5a1045ce36f42989fb198
Hosein Roshanayi <Roshanayixyz@gmail.com>
2022-09-28 17:22:00 +0330

    end

--- 02-variables-strings-numbers/app.js
+++ 02-variables-strings-numbers/app.js
@@ -1,2 +1,24 @@
-let firstName = 'Hosein Roshanayi'
-console.log(firstName)
+// String
+
+let firstName = 'Hosein'
+let lastName = 'Roshanayi'
+let fullName = firstName + ' ' + lastName
+console.log(fullName)
+
+
+
+// Number
+
+let num1 = 12
+let num2 = 20
+let sum = num1 + num2
+
+console.log(sum)
+
+let res = fullName + num1 + num2 
+
+console.log(res)
+
+
+
+
